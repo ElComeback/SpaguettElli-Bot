0a79ebec84f54f1e85350f709947b2a863c81094
Fail69 <itrolgamer@gmail.com>
2018-03-06 16:01:55 -0300

    Update spaelli.js

--- spaelli.js
+++ spaelli.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const prefix = process.env.PREFIX
 
 client.on("ready", () => {
    console.log("Estoy Online!");
@@ -8,9 +9,9 @@ client.on("ready", () => {
 
 client.on('message', message => {
     if (message.author.bot) return;
-    if (!message.content.startsWith(process.env.PREFIX)) return;
+    if (!message.content.startsWith(prefix)) return;
 
-    if (message.content.startsWith(process.env.PREFIX + 'ping')) {
+    if (message.content.startsWith(prefix + 'ping')) {
         message.channel.sendMessage('Pong! Your ping is `' + `${Date.now() - message.createdTimestamp}` + ' ms`');
     }
 });
