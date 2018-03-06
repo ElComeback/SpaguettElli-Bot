6a5881a36f67c3821143026a0eb50bc3b862e110
ElComeback <34289220+ElComeback@users.noreply.github.com>
2018-03-06 14:14:21 -0600

    Update spaelli.js

--- spaelli.js
+++ spaelli.js
@@ -12,16 +12,3 @@ new Client({
     typing: true,
     readyMessage: (client) => `${client.user.tag}, Online en ${client.guilds.size} servidores y con ${client.users.size} usuarios`
 }).login('token');
-
-client.on("ready", () => {
-   console.log("Estoy Online!");
-});
- //Ping--------------------------------------------------------------------------------------------------------------
-client.on('message', message => {
-    if (message.author.bot) return;
-    if (!message.content.startsWith(prefix)) return;
-
-    if (message.content.startsWith(prefix + 'ping')) {
-        message.channel.sendMessage('Pong! Your ping is `' + `${Date.now() - message.createdTimestamp}` + ' ms`');
-    }
-});   
