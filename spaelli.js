4bcd831cc9deb379ed8187eaf43a6da0a2f8a4c8
Fail69 <itrolgamer@gmail.com>
2018-03-06 15:21:15 -0300

    Update spaelli.js

--- spaelli.js
+++ spaelli.js
@@ -1,12 +1,18 @@
 const Discord = require("discord.js");
-const  client = new Discord.Client();
+const client = new Discord.Client();
 
 client.on("ready", () => {
    console.log("Estoy Online!");
 });
-client.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
-    message.channel.send("pong!");
-  }
+
+
+client.on('message', message => {
+    if (message.author.bot) return;
+    if (!message.content.startsWith(process.env.PREFIX)) return;
+
+    if (message.content.startsWith(process.env.PREFIX + 'ping')) {
+        message.channel.sendMessage('Pong! Your ping is `' + `${Date.now() - message.createdTimestamp}` + ' ms`');
+    }
 });
+
 client.login(process.env.BOT_TOKEN);     
