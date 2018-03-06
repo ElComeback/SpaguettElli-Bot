2d7fa3b93b6c99928e37f2043c04ff6d2eb61be2
Fail69 <itrolgamer@gmail.com>
2018-03-06 18:46:44 -0300

    Update purge.js

--- commands/Misc/purge.js
+++ commands/Misc/purge.js
@@ -15,17 +15,17 @@ module.exports = class extends Command {
             requiredSettings: [],
             description: 'Elimina una cantidad de mensajes dado por el usuario',
             quotedStringSupport: false,
-            usage: '<amount:int{2,100}>',
+            usage: '<amount:int{2,100}> [messsage:str] [...]',
             usageDelim: undefined,
             extendedHelp: 'No extended help available.'
         });
     }
 
-    async run(msg, [amount]) {
+    async run(msg, [amount, message]) {
         return msg.channel.bulkDelete(amount, true)
-        let message = await msg.channel.send(`***__Eliminando ${args} mensajes. Por favor espere, esto podría tomar un rato...__***`)
-        let modLog = msg.guild.channels.find('name', 'log')
-        msg.channel.send(`:wastebasket: | **__${args} mensajes eliminados correctamente!__**`);
+        const message = return await msg.channel.send(`***__Eliminando ${args} mensajes. Por favor espere, esto podría tomar un rato...__***`)
+        const modLog = msg.guild.channels.find('name', 'log')
+        return msg.channel.send(`:wastebasket: | **__${args} mensajes eliminados correctamente!__**`);
         message.delete();
         msg.delete();
         const embed = new Discord.RichEmbed()
@@ -33,7 +33,7 @@ module.exports = class extends Command {
        .setTimestamp()
        .addField('Action:', '***purge***')
        .addField('Purged By:', `${msg.author}`)
-       .addField('Purged Messages:', `**${args}**`)
+       .addField('Purged Messages:', `**${message}**`)
        .setColor('RANDOM')
        .setFooter(`ServerID: ${msg.guild.id}`)
        return msg.guild.channels.get(modLog.id).send({
