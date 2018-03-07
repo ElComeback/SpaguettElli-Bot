36704008d0ab897e81d3b1765ab4ed41ce197736
Fail69 <itrolgamer@gmail.com>
2018-03-07 14:54:11 -0300

    Update purge.js

--- commands/Misc/purge.js
+++ commands/Misc/purge.js
@@ -5,37 +5,44 @@ const Discord = require('discord.js');
 module.exports = class extends Command {
     constructor(...args) {
         super(...args, {
-            name: 'purge',
+            name: 'prune',
             enabled: true,
             runIn: ['text', 'group'],
             cooldown: 5,
-            aliases: ['pg'],
+            aliases: ['pr'],
             permLevel: 6,
             botPerms: ['MANAGE_MESSAGES'],
             requiredSettings: [],
             description: 'Elimina una cantidad de mensajes dada por el usuario',
             quotedStringSupport: false,
-            usage: '<amount:int{2,100}>',
+            usage: '[limit:integer] [link|invite|bots|you|me|upload|user:user]',
             usageDelim: undefined,
             extendedHelp: 'No extended help available.'
         });
     }
 
-    async run(msg, [amount, message]) {
-       return (msg.channel.bulkDelete(amount, true), msg.send(`**__¡${amount} mensajes eliminados con éxito!__**`));
-       msg.delete();
-       const embed = new this.client.methods.Embed()
-       .addTitle('Action:', '***purge***')
-       .addField('Purged By:', `${this.msg.author.username}`)
-       .addField('Purged Messages:', `**${amount}**`)
-       .addField('Purged By:', `${this.msg.author.username}`, true)
-       .addField('Purged Messages:', `**${amount}**`, true)
-       .setColor('RANDOM')
-       .setTimestamp()
-       .setFooter(`ServerID:, ${msg.guild.id}`)
-       .setFooter(`ServerID: ${msg.guild.id}`)
-       return this.client.channels.get('399062303980519434').send({ embed });
-    }
-    async init() {
-    }
+    async run(msg, [limit = 50, filter = null]) {
+		let messages = await msg.channel.messages.fetch({ limit: 100 });
+		if (filter) {
+			const user = typeof filter !== 'string' ? filter : null;
+			const type = typeof filter === 'string' ? filter : 'user';
+			messages = messages.filter(this.getFilter(msg, type, user));
+		}
+		messages = messages.array().slice(0, limit);
+		await msg.channel.bulkDelete(messages);
+		return msg.send(`Successfully deleted ${messages.length} messages from ${limit}.`);
+	}
+
+	getFilter(msg, filter, user) {
+		switch (filter) {
+			case 'link': return mes => /https?:\/\/[^ /.]+\.[^ /.]+/.test(mes.content);
+			case 'invite': return mes => /(https?:\/\/)?(www\.)?(discord\.(gg|li|me|io)|discordapp\.com\/invite)\/.+/.test(mes.content);
+			case 'bots': return mes => mes.author.bot;
+			case 'you': return mes => mes.author.id === this.client.user.id;
+			case 'me': return mes => mes.author.id === msg.author.id;
+			case 'upload': return mes => mes.attachments.size > 0;
+			case 'user': return mes => mes.author.id === user.id;
+			default: return () => true;
+		}
+}
 };
