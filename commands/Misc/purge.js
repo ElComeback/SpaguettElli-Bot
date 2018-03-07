fac2e5f4c18aa8805e3803a05a7c81f28bf18747
Fail69 <itrolgamer@gmail.com>
2018-03-07 14:06:37 -0300

    Update purge.js

--- commands/Misc/purge.js
+++ commands/Misc/purge.js
@@ -21,8 +21,9 @@ module.exports = class extends Command {
         });
     }
 
-    async run(msg, [amount, message, client]) {
+    async run(msg, [amount, message]) {
        return (msg.channel.bulkDelete(amount, true), msg.send(`**__¡${amount} mensajes eliminados con éxito!__**`));
+       msg.delete();
        const embed = new this.client.methods.Embed()
        .addTitle('Action:', '***purge***')
        .addField('Purged By:', `${this.msg.author.username}`)
@@ -34,7 +35,6 @@ module.exports = class extends Command {
        .setFooter(`ServerID:, ${msg.guild.id}`)
        .setFooter(`ServerID: ${msg.guild.id}`)
        return this.client.channels.get('399062303980519434').send({ embed });
-       msg.delete();
     }
     async init() {
     }
