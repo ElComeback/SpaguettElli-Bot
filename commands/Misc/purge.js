fce7d716f51603f457713aad1859580e715306ee
Fail69 <itrolgamer@gmail.com>
2018-03-06 21:47:01 -0300

    Update purge.js

--- commands/Misc/purge.js
+++ commands/Misc/purge.js
@@ -32,7 +32,7 @@ module.exports = class extends Command {
        .addField('Purged Messages:', `**${amount}**`)
        .setColor('RANDOM')
        .setFooter(`ServerID: ${msg.guild.id}`)
-       return msg.send(modLog, { embed });
+       return msg.modLog.send({ embed });
     }
     async init() {
     }
