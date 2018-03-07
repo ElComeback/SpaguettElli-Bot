82ddefd204418b2159f374fd572d10114de662e7
Fail69 <itrolgamer@gmail.com>
2018-03-06 23:23:10 -0300

    Update purge.js

--- commands/Misc/purge.js
+++ commands/Misc/purge.js
@@ -24,18 +24,6 @@ module.exports = class extends Command {
     async run(msg, [amount, message]) {
         return (msg.channel.bulkDelete(amount, true), msg.send(`**__¡${amount} mensajes eliminados con éxito!__**`));
         msg.delete();
-        await this.msg.guild.configs.update('modlogs', action, this.msg.guild)
-        const { modlogChannel }  = this.msg.guild.configs
-
-        if (modlogChannel) {
-       const embed = new this.client.methods.Embed()
-       .setTimestamp()
-       .addField('Action:', '***purge***')
-       .addField('Purged By:', `${this.msg.author.username}`, true)
-       .addField('Purged Messages:', `**${amount}**`, true)
-       .setColor('RANDOM')
-       .setFooter(`ServerID: ${msg.guild.id}`)
-       return this.client.channels.get(modlogChannel).send({ embed })
     }
     async init() {
     }
