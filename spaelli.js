b54c80cc269568a4c4eea24b57b9583e4aa9fea4
Fail69 <itrolgamer@gmail.com>
2018-03-06 23:55:17 -0300

    Update spaelli.js

--- spaelli.js
+++ spaelli.js
@@ -1,7 +1,5 @@
-const Discord = require("discord.js");
-const prefix = process.env.PREFIX;
 const token = process.env.BOT_TOKEN;
-const { Client } = require('klasa');
+const {Client, PermissionLevels} = require('klasa');
 //Inicio de Klasa--------------------------------------------------------------------------------------------------
 new Client({
     clientOptions: {
@@ -16,3 +14,22 @@ new Client({
     botDevs: ['131403526411780096', '285402109900226560'],
     readyMessage: (client) => `${client.user.tag}, Online en ${client.guilds.size} servidores y con ${client.users.size} usuarios`
 }).login(token);
+//Inicio de PermLevels-----------------------------------------------------------------------------------------------
+client.eventArray = []
+client.usersArray = client.users.array()
+
+client.permissionLevels = new PermissionLevels()
+  // Cualquiera puede usar este comando
+  .add(0, () => true)
+  // Miembros que sean mods en el servidor
+  .add(7, (client, msg) => {
+    if (!msg.guild) return false
+    if (msg.member.roles.get(msg.guildConfigs.modRole)) return true
+    return false
+  })
+  // Miembros que son owners en el servidor
+  .add(8, (client, msg) => msg.guild && msg.member === msg.guild.owner)
+  // Bot Developers
+  .add(9, (client, msg) => client.botDevs.includes(msg.author.id))
+  // Deja al creador del bot usar comandos solo para el creador del bot
+.add(10, (client, msg) => msg.author === client.owner)
