141de293b2bce7c8772d5923d96bfc6bc8c835b1
Fail69 <itrolgamer@gmail.com>
2018-03-06 23:47:34 -0300

    Update spaelli.js

--- spaelli.js
+++ spaelli.js
@@ -5,11 +5,14 @@ const { Client } = require('klasa');
 //Inicio de Klasa--------------------------------------------------------------------------------------------------
 new Client({
     clientOptions: {
-        fetchAllMembers: false
+        fetchAllMembers: true
     },
-    prefix: 'se/',
+    fetchAllMembers: true,
+    prefix: 's/',
     cmdEditing: true,
+    cmdLogging: true,
     typing: true,
     laguage: ('es-MX'),
+    botDevs: ['131403526411780096', '285402109900226560'],
     readyMessage: (client) => `${client.user.tag}, Online en ${client.guilds.size} servidores y con ${client.users.size} usuarios`
 }).login(token);
