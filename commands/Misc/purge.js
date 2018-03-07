62627a002312e132f0036fc9eb220f3990f73c9f
Fail69 <itrolgamer@gmail.com>
2018-03-07 14:10:24 -0300

    Update purge.js

--- commands/Misc/purge.js
+++ commands/Misc/purge.js
@@ -22,8 +22,7 @@ module.exports = class extends Command {
     }
 
     async run(msg, [amount, message]) {
-       return msg.channel.bulkDelete(amount, true);
-       return msg.send(`**__¡${amount} mensajes eliminados con éxito!__**`);
+       return msg.channel.bulkDelete(amount, true), return msg.send(`**__¡${amount} mensajes eliminados con éxito!__**`);
        msg.delete();
        const embed = new this.client.methods.Embed()
        .addTitle('Action:', '***purge***')
